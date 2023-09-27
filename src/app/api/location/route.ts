6fe9a1fede0f217ea3fc539900e058a05575f484
George Yin Xu <89299295+georgeyinxu@users.noreply.github.com>
2023-09-27 09:33:50 +0800

    added api for all the countries

---
+++ src/app/api/location/route.ts
@@ -0,0 +1,20 @@
+import { ILocation } from "@/interfaces/LocationResponse";
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  let officialNames: string[] = [];
+  try {
+    const data = await axios.get("https://restcountries.com/v3.1/all");
+    const countries = data.data as ILocation[];
+
+    officialNames = countries.map(country => country.name.common).sort();
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Failed to get all countries" },
+      { status: 400 }
+    );
+  }
+
+  return NextResponse.json({ data: officialNames }, { status: 200 });
+}
