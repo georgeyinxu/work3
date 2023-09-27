98185308aa4a13adb7b6b22bdeb8a5045f96190b
George Yin Xu <89299295+georgeyinxu@users.noreply.github.com>
2023-09-27 16:23:45 +0800

    added the loading portion of the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
 
 import { ICategory } from "@/interfaces/CategoryResponse";
 import { IListing } from "@/interfaces/ListingResponse";
@@ -9,13 +8,17 @@ import { IListing } from "@/interfaces/ListingResponse";
 import { fetchListings } from "@/utils/Listings";
 import { fetchCategories } from "@/utils/Categories";
 import ListingCard from "@/components/ListingCard";
+import Category from "@/components/Loading/Category";
+import ListingCardLoad from "@/components/Loading/ListingCard";
 
 const App = () => {
   const [categories, setCategories] = useState<ICategory[]>([]);
   const [listings, setListings] = useState<IListing[]>([]);
   const [selected, setSelected] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const promises = [fetchCategories(), fetchListings()];
 
     Promise.all(promises).then((responses) => {
@@ -24,6 +27,7 @@ const App = () => {
 
       setCategories(categories);
       setListings(listings);
+      setIsLoading(false);
     });
   }, []);
 
@@ -37,6 +41,7 @@ const App = () => {
         <h1 className="font-extrabold text-transparent text-2xl sm:text-3xl md:text-4xl lg:text-6xl bg-clip-text bg-gradient-to-tr from-[#FF4FB8] to-[#FF9FFB]">
           [How Work Should Work]
         </h1>
+        {isLoading && <Category />}
         <div className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-8 gap-2 md:gap-4 mt-10">
           {categories &&
             categories.map((category) => (
@@ -59,13 +64,16 @@ const App = () => {
         </div>
         <hr className="w-full h-0.5 bg-gradient-to-r from-[#ff00c7] to-[#ff9bfb] rounded-full my-10" />
         <div className="flex flex-col gap-4 w-full">
+          {isLoading && <ListingCardLoad />}
           {listings &&
             listings.map((listing) => {
               if (
                 listing.category === selected ||
                 categories[0]._id === selected
               ) {
-                return <ListingCard listingDetails={listing} key={listing._id} />;
+                return (
+                  <ListingCard listingDetails={listing} key={listing._id} />
+                );
               }
             })}
         </div>
---
+++ src/components/Loading/Category.tsx
@@ -0,0 +1,78 @@
+const Category = () => {
+  return (
+    <div className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-8 gap-2 md:gap-4 mt-10">
+      <div className="border border-gray-400 rounded-full p-4">
+        <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+          <div className="flex items-center w-full space-x-2">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+      <div className="border border-gray-400 rounded-full p-4">
+        <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+          <div className="flex items-center w-full space-x-2">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+      <div className="border border-gray-400 rounded-full p-4">
+        <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+          <div className="flex items-center w-full space-x-2">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+      <div className="border border-gray-400 rounded-full p-4">
+        <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+          <div className="flex items-center w-full space-x-2">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+      <div className="border border-gray-400 rounded-full p-4">
+        <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+          <div className="flex items-center w-full space-x-2">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+      <div className="border border-gray-400 rounded-full p-4">
+        <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+          <div className="flex items-center w-full space-x-2">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+      <div className="border border-gray-400 rounded-full p-4">
+        <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+          <div className="flex items-center w-full space-x-2">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
