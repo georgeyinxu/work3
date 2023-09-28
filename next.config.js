4068299bd5a01eca759fda2528986136c711597f
George Yin Xu <89299295+georgeyinxu@users.noreply.github.com>
2023-09-28 12:06:18 +0800

    reverted changes

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config, options) {
-    const externals = {
-      ...config.externals,
-      bcrypt: "bcrypt",
-      jimp: "jimp",
-      "probe-image-size": "probe-image-size",
-    };
-    return {
-      ...config,
-      module: {
-        ...config.module,
-        rules: [...config.module.rules, ...rules],
-      },
-      externals,
-    };
-  },
 };
 
 module.exports = nextConfig;
