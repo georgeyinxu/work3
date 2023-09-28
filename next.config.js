98d3dcd3d3a4bf2c97bd2fd8d0f104c11782c051
George Yin Xu <89299295+georgeyinxu@users.noreply.github.com>
2023-09-28 12:02:30 +0800

    testing fix for build errors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config, options) {
+    const externals = {
+      ...config.externals,
+      bcrypt: "bcrypt",
+      jimp: "jimp",
+      "probe-image-size": "probe-image-size",
+    };
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [...config.module.rules, ...rules],
+      },
+      externals,
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
