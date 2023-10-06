dfb7e3cb6459b988dce46e8d4defb0039db69ef8
George Yin Xu <89299295+georgeyinxu@users.noreply.github.com>
2023-10-06 10:03:02 +0800

    checkpoint for 6 oct

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -7,21 +7,21 @@ import { IMessage } from "@/interfaces/Message";
 
 const Chat = () => {
   const [chatMessages, setChatMessages] = React.useState<IMessage[]>([]);
-  // let socket: any;
-  // socket = io("http://localhost:3001");
+  let socket: any;
+  socket = io("http://localhost:3001");
 
-  // useEffect(() => {
-  //   socket.on("receive_msg", (data: IMessage) => {
-  //     setChatMessages((prev) => [...prev, data]);
-  //   });
-  // }, [socket]);
+  useEffect(() => {
+    socket.on("receive_msg", (data: IMessage) => {
+      setChatMessages((prev) => [...prev, data]);
+    });
+  }, [socket]);
 
   return (
     <div className="max-w-sm mx-auto mt-4">
       <div className="bg-white border border-gray-200 rounded-lg shadow relative">
         <ChatHeader name={"devlazar"} numberOfMessages={4} />
         <ChatContent messages={chatMessages} />
-        {/* <ChatInputBox socket={socket} roomId={4} /> */}
+        <ChatInputBox socket={socket} roomId={4} />
       </div>
     </div>
   );
